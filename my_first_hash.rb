dcd00a9b005064e9365e25ceb9bb9f970ac0ebfa
Abel Kalum <chumbabel@gmail.com>
2020-01-25 23:59:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,7 +30,6 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-   hash["new_key"] = "New Value"              
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
 
