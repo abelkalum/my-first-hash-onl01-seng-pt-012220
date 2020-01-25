1c88b5db13eaaf55f366422fdfc9ffb1335d455b
Abel Kalum <chumbabel@gmail.com>
2020-01-25 23:53:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -33,14 +33,5 @@ def adding
    hash["new_key"] = "New Value"              
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
