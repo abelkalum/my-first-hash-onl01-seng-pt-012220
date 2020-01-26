d6a723f9668d64c080e6d0cc3371fe3f5f4836f3
Abel Kalum <chumbabel@gmail.com>
2020-01-26 01:43:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,5 +31,5 @@ def adding
                   "oil paintings" => 3, 
                  }
   shipping_manifest.merge! :"muskets" => 2, :"gun powder" => 4
-  puts shipping_manifest.reverse
+  puts shipping_manifest
 end
